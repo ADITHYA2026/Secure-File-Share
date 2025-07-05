c295da6c69c13042a5bd2c66cd277490a70bcd02
Adithya Prathi <adityaprathi2005@gmail.com>
2025-07-05 20:59:52 +0530

    updated storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,19 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "securefilesharefb45605c": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "securefilestorage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
